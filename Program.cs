1009204b92a8cf539528359eb80df6fcc7b24377
Liene <lieneirkle@gmail.com>
2022-09-14 11:23:34 -0400

    .

--- Program.cs
+++ Program.cs
@@ -23,3 +23,22 @@ bool IsCharUnique(String str)
 Console.WriteLine(IsCharUnique("122bcd"));
 
 
+// check if string is a permutation of the other
+
+String SortString(String str)
+{
+    char[] content = str.ToCharArray();
+    Array.Sort(content);
+    return new String(content);
+}
+
+bool Permutation(String str1, String str2)
+{
+    if (str1.Length != str2.Length)
+    {
+        return false;
+    }
+    return SortString(str1).Equals(SortString(str2));
+}
+
+Console.WriteLine(Permutation("abcv", "cbas"));
