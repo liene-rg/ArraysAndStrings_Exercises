4dd1d88c063d5026a4242ec16579a42e3e1edd59
Liene <lieneirkle@gmail.com>
2022-09-14 10:57:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+// Check if string has only unique values
+bool IsCharUnique(String str)
+{
+    if (str.Length > 128) // assuming 128 ASCII 
+    {
+        return false;
+    }
+    bool[] charSet = new bool[128];
+    for (int i = 0; i < str.Length; i++)
+    {
+        int val = str.ElementAt(i);
+        if (charSet[val])
+        {
+            return false;
+        }
+        charSet[val] = true;
+    }
+    return true;
+}
+
+Console.WriteLine(IsCharUnique("122bcd"));
+
+
